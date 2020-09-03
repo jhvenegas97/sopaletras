d74ff67ca3090d4d883dcd8a3d513a4352a9160c
Jonathan Venegas <jonathan.9129@gmail.com>
2020-09-03 15:12:24 -0500

    Primer Commit

---
+++ letras.js
@@ -0,0 +1,52 @@
+var frase1 = {
+	letra1 : "f1le1",
+	letra2 : "f1ln",
+	letra3 : "f1ll",
+	letra4 : "f1la",
+	letra5 : "f1lc",
+	letra6 : "f1le2",
+	letra7 : "f1ls"
+}
+
+var frase2 = {
+	letra1 : "f2ls",
+	letra2 : "f2lp",
+	letra3 : "f2la",
+	letra4 : "f2lm"
+}
+
+var frase3 = {
+	letra1 : "f3lc",
+	letra2 : "f3lo",
+	letra3 : "f3lm",
+	letra4 : "f3lp",
+	letra5 : "f3la1",
+	letra6 : "f3lr",
+	letra7 : "f3lt",
+	letra8 : "f3la2",
+	letra9 : "f3ls"
+}
+
+var frase4 = {
+	letra1 : "f4lr",
+	letra2 : "f4le",
+	letra3 : "f4lg",
+	letra4 : "f4la",
+	letra5 : "f4ll",
+	letra6 : "f4lo",
+	letra7 : "f4ls"
+}
+
+var frase5 = {
+	letra1 : "f5lc",
+	letra2 : "f5lo",
+	letra3 : "f5ln",
+	letra4 : "f5lt",
+	letra5 : "f5lr",
+	letra6 : "f5la1",
+	letra7 : "f5ls",
+	letra8 : "f5le",
+	letra9 : "f5ln1",
+	letra10 : "f5la2",
+	letra11 : "f5ls1"
+}
